d85a2cdfa35f5884a29028ca8dda9a72a1c3e9c9
himanshutiwary-gcp <himanshutiwary666@gmail.com>
2025-07-25 11:50:39 +0530

    Update auth.routes.ts

--- nova-backend/src/api/auth/auth.routes.ts
+++ nova-backend/src/api/auth/auth.routes.ts
@@ -2,4 +2,5 @@ import { Router } from 'express';
 import { handleGoogleLogin } from './auth.controller';
 const router = Router();
 router.post('/google', handleGoogleLogin);
+router.post('/register', handleRegister);
 export default router;
